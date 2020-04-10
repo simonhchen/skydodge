debbc1a9f0a98f6a9cd4f78a95bde67f739c25d1
Simon Chen <simonhchen@gmail.com>
2020-04-10 14:35:13 -0700

    tutorial 3.1 skydodge.py commit

--- skydodge.py
+++ skydodge.py
@@ -13,16 +13,28 @@ from pygame.locals import (
     QUIT
 )
 
-# Intialize pygame
-pygame.init()
-
 # Define constants for the screen width and height
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+# Define a player object by extending pygame.sprite.SPrite
+# The sruface dawn on the screen is now an attribute of "player"
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+# Intialize pygame
+pygame.init()
+
 # Create the screen object
 # The size is determined by constants SCREEN_WIDTH AND SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instantiate the player. Will be rectangle initially
+player = Player()
 
 # Variable to keep the main loop running
 running = True
@@ -36,27 +48,15 @@ while running:
                 # Was it the ESCAPE key? If so, stop the loop
                 if event.key == K_ESCAPE:
                     running = False
-
             # Did the user click the window close button? If so, stop the loop.
             elif event.type == QUIT:
                 running = False
 
-    # Fill the screen with white
-    screen.fill((255, 255, 255))
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
 
-    # Create a surface and pass a tuple containing its length and width
-    surf = pygame.Surface((50,50))
+    # Draw the player on the screent
+    screen.blit(player.surf, player.rect)
 
-    # Give the surface a color to separate it from the background
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-
-    # Put the center of surf at the center of the display
-    surf_center = (
-        (SCREEN_WIDTH-surf.get_width())/2,
-        (SCREEN_HEIGHT-surf.get_height())/2
-    )
-
-    # This line says "Draw surf onto the screen at the center"
-    screen.blit(surf, surf_center)
+    # Update the display
     pygame.display.flip()
