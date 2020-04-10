1eefad60b46fcd7aa7ae5b173a7641f1d9d17fed
Simon Chen <simonhchen@gmail.com>
2020-04-10 14:49:15 -0700

    tutorial 3.3 skydodge.py commit

--- skydodge.py
+++ skydodge.py
@@ -37,6 +37,17 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
 
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
 # Intialize pygame
 pygame.init()
 
