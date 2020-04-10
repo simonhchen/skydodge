11df3bddd46b8c4e8f71772a964078b785b022a3
Simon Chen <simonhchen@gmail.com>
2020-04-10 14:11:06 -0700

    tutorial ch.8 skydodge.py commit

--- skydodge.py
+++ skydodge.py
@@ -23,3 +23,20 @@ SCREEN_HEIGHT = 600
 # Create the screen object
 # The size is determined by constants SCREEN_WIDTH AND SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+            if event.type == KEYDOWN:
+                # Was it the ESCAPE key? If so, stop the loop
+                if event.key == K_ESCAPE:
+                    running = False
+
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == QUIT:
+                running = False
